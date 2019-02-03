995457916c621dcf3e590a6c6a7987d0b70f4172
akhilg18 <akhileshg@mail.usf.edu>
2019-02-03 14:23:17 -0500

    update1

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -27,8 +27,8 @@ namespace Assignment2_S19
 
             // Missing numbers
             Console.WriteLine("\n\nMissing numbers");
-            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
-            int[] brr = {203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204};
+            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+            int[] brr = { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
             int[] r2 = missingNumbers(arr1, brr);
             displayArray(r2);
 
@@ -38,9 +38,10 @@ namespace Assignment2_S19
             int[] r3 = gradingStudents(grades);
             displayArray(r3);
 
+
             // find the median
             Console.WriteLine("\n\nFind the median");
-            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3};
+            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
             Console.WriteLine(findMedian(arr2));
 
             // closest numbers
@@ -53,11 +54,13 @@ namespace Assignment2_S19
             Console.WriteLine("\n\nDay of Programmer");
             int year = 2017;
             Console.WriteLine(dayOfProgrammer(year));
+            Console.ReadLine();
+
         }
 
-        static void displayArray(int []arr) {
+        static void displayArray(int[] arr) {
             Console.WriteLine();
-            foreach(int n in arr) {
+            foreach (int n in arr) {
                 Console.Write(n + " ");
             }
         }
@@ -65,12 +68,30 @@ namespace Assignment2_S19
         // Complete the rotLeft function below.
         static int[] rotLeft(int[] a, int d)
         {
-            return new int[] {};
+            return new int[] { };
         }
 
         // Complete the maximumToys function below.
         static int maximumToys(int[] prices, int k)
         {
+            try
+            {
+                int maxToys = 0;
+                Array.Sort(prices);
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    if (prices[i] < k)
+                    {
+                        maxToys++;
+                    }
+                    k = k - prices[i];
+                }
+                return maxToys;
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing maximumToys()");
+            }
             return 0;
         }
 
@@ -87,11 +108,33 @@ namespace Assignment2_S19
         }
 
 
+
         // Complete the gradingStudents function below.
         static int[] gradingStudents(int[] grades)
         {
-            return new int[] { };
+            try
+            {
+                for (int i = 0; i < grades.Length; i++)
+                {
+                    int temp = 0;
+                    if (grades[i] >= 38)
+                    {
+                        temp = grades[i] / 5;
+                        if (((temp + 1) * 5) - grades[i] < 3)
+                        {
+                            grades[i] = (temp + 1) * 5;
+                        }
+                    }
+                }
+                return grades;
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing gradingStudents()");
+            }
+            return null;
         }
+
 
         // Complete the findMedian function below.
         static int findMedian(int[] arr)
